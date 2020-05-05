7f4b7c501d9033a195b0e18dd7c450d2468d4bae
cyntiafanny <cyntia.fanny@student.umn.ac.id>
2020-05-06 01:18:51 +0700

    - login - signup - fragment layout custom

--- app/src/main/java/com/labill/frasaapp/ui/landing/LandingActivity.java
+++ app/src/main/java/com/labill/frasaapp/ui/landing/LandingActivity.java
@@ -2,15 +2,34 @@ package com.labill.frasaapp.ui.landing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.labill.frasaapp.R;
+import com.labill.frasaapp.ui.login_and_signup.LoginActivity;
+import com.labill.frasaapp.ui.login_and_signup.SignUpActivity;
 
 public class LandingActivity extends AppCompatActivity {
+
+    private ImageButton start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        start = findViewById(R.id.login);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startNow = new Intent(LandingActivity.this, LoginActivity.class);
+                startActivity(startNow);
+                finish();
+
+            }
+        });
     }
 }
