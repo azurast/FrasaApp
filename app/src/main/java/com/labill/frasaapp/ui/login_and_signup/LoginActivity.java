0353bc097a9f2867b1fec9b50b47fedfd28ee72e
Azura Sakan Taufik <38053935+azurast@users.noreply.github.com>
2020-05-12 09:18:44 +0700

    Revert "Fani"

--- app/src/main/java/com/labill/frasaapp/ui/login_and_signup/LoginActivity.java
+++ app/src/main/java/com/labill/frasaapp/ui/login_and_signup/LoginActivity.java
@@ -1,108 +1,16 @@
 package com.labill.frasaapp.ui.login_and_signup;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.labill.frasaapp.R;
 
 public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-    private EditText etUsername, etPassword;
-    private TextView regist;
-    private ImageButton login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        etUsername = findViewById(R.id.email);
-        etPassword = findViewById(R.id.password);
-
-        regist = findViewById(R.id.move2);
-
-        regist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goRegister = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(goRegister);
-                finish();
-
-            }
-        });
-
-        login = findViewById(R.id.login);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String username = etUsername.getText().toString();
-                String password = etPassword.getText().toString();
-
-                if (username.equals("")){
-                    Toast.makeText(LoginActivity.this, "Email can't be empty", Toast.LENGTH_SHORT).show();
-
-                } else if (password.equals("")){
-                    Toast.makeText(LoginActivity.this, "Password can't be empty", Toast.LENGTH_SHORT).show();
-
-                }
-
-                else {
-
-                    mAuth.signInWithEmailAndPassword(username, password)
-                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        // Sign in success, update UI with the signed-in user's information\
-                                        FirebaseUser user = mAuth.getCurrentUser();
-
-                                        Toast.makeText(LoginActivity.this, "Authentication Success",
-                                                Toast.LENGTH_SHORT).show();
-
-
-                                    }
-
-                                    else {
-                                        // If sign in fails, display a message to the user.
-                                        Toast.makeText(LoginActivity.this, "Email or Password wrong",
-                                                Toast.LENGTH_SHORT).show();
-                                    }
-
-                                    // ...
-                                }
-                            });
-
-                }
-
-            }
-        });
-
-
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
     }
 }
