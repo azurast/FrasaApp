036a94a5166d9b14e9758bfc7387561d88e1cf2f
cyntiafanny <cyntia.fanny@student.umn.ac.id>
2020-05-23 20:53:31 +0700

    -fixed post gap

--- app/src/main/java/com/labill/frasaapp/ui/profile/Profile2Activity.java
+++ app/src/main/java/com/labill/frasaapp/ui/profile/Profile2Activity.java
@@ -1,14 +1,29 @@
 package com.labill.frasaapp.ui.profile;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.labill.frasaapp.R;
+import com.squareup.picasso.Picasso;
 
 public class Profile2Activity extends AppCompatActivity {
 
@@ -16,6 +31,13 @@ public class Profile2Activity extends AppCompatActivity {
     private ViewPager viewPager;
     private TabItem postTab, followerTab, followingTab;
     public PagerAdapter pagerAdapter;
+    FirebaseFirestore db;
+    FirebaseAuth mAuth;
+    StorageReference references;
+
+    private ImageView pp;
+    private TextView name, bio;
+    private String id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +49,35 @@ public class Profile2Activity extends AppCompatActivity {
         followerTab = (TabItem) findViewById(R.id.followerTab);
         followingTab = (TabItem) findViewById(R.id.followingTab);
         viewPager = findViewById(R.id.viewPager);
+        pp = findViewById(R.id.profile_image);
+        name = findViewById(R.id.tvAuthorName);
+        bio = findViewById(R.id.tvAuthorBio);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        references = FirebaseStorage.getInstance().getReference();
+        id = mAuth.getCurrentUser().getUid();
+
+        StorageReference profileRef = references.child("users/"+id+"/user.jpg");
+
+        final DocumentReference documentReference = db.collection("users").document(id);
+
+        documentReference.addSnapshotListener(Profile2Activity.this, new EventListener<DocumentSnapshot>()
+        {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                name.setText(documentSnapshot.getString("name"));
+                bio.setText(documentSnapshot.getString("bio"));
+
+            }
+        });
+
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(pp);
+            }
+        });
 
         pagerAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(pagerAdapter);
