0d0fd45121ab67c077618b38ae54dce3083a0ab9
cyntiafanny <cyntia.fanny@student.umn.ac.id>
2020-05-31 14:01:11 +0700

    see user fixed

--- app/src/main/java/com/labill/frasaapp/ui/reading_mode/ReadingActivity.java
+++ app/src/main/java/com/labill/frasaapp/ui/reading_mode/ReadingActivity.java
@@ -31,6 +31,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.labill.frasaapp.R;
+import com.labill.frasaapp.ui.profile.Profile2Activity;
 import com.labill.frasaapp.ui.profile.SeeProfile;
 
 import javax.annotation.Nullable;
@@ -331,9 +332,16 @@ public class ReadingActivity extends AppCompatActivity {
         tvAuthor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ReadingActivity.this, SeeProfile.class);
-                intent.putExtra("id", idAuthor);
-                startActivity(intent);
+                if(idAuthor.equals(mAuth.getCurrentUser().getUid()))
+                {
+                    Intent intent = new Intent(ReadingActivity.this, Profile2Activity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Intent intent = new Intent(ReadingActivity.this, SeeProfile.class);
+                    intent.putExtra("id", idAuthor);
+                    startActivity(intent);
+                }
             }
         });
 
